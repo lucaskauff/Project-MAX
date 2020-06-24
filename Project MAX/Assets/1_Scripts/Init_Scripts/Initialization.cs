c65ee6520d15eb0d12dae6a5cbeccdc49f75e5a9
Lucas Kauff <l.kauff@rubika-edu.com>
2020-06-25 00:44:30 +0200

    Plugins added + Init Scene created

---
+++ Project MAX/Assets/1_Scripts/Init_Scripts/Initialization.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Max
+{
+    public class Initialization : MonoBehaviour
+    {
+        #region References
+        [SerializeField] private Image pcBackgroundImage = default;
+        [SerializeField] private Image switchImage = default;
+        [SerializeField] private Animator logoImage = default;
+        #endregion
+
+        #region Variables
+        [SerializeField] private string nextSceneName = default;
+
+        [Header("Colors")]
+        [SerializeField] private Color pcBackgroundColorOff = default;
+        [SerializeField] private Color pcBackgroundColorOn = default;
+        [SerializeField] private Color switchColorOn = default;
+        [SerializeField] private Color switchColorOff = default;
+        #endregion
+
+        #region Callbacks
+        private void Start()
+        {
+            
+        }
+        #endregion
+
+        #region Utilities
+
+        #endregion
+    }
+}
---
+++ Project MAX/Assets/1_Scripts/Managers/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Max
+{
+    public class GameManager : Singleton<GameManager>
+    {
+        protected GameManager() { }
+
+        #region References
+
+        #endregion
+
+        #region Variables
+
+        #endregion
+
+        #region Callbacks
+
+        #endregion
+
+        #region Utilities
+
+        #endregion
+    }
+}
